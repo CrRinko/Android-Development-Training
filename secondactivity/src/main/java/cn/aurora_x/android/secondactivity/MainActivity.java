e14dafa42fa7609dea1d6a2de73c944ef554df3a
Rinko <crimson_rin@yeah.net>
2016-08-31 10:30:23 +0800

    Create secondactivity module to practice developing the multi-activity program.

--- secondactivity/src/main/java/cn/aurora_x/android/secondactivity/MainActivity.java
+++ secondactivity/src/main/java/cn/aurora_x/android/secondactivity/MainActivity.java
@@ -1,7 +1,9 @@
 package cn.aurora_x.android.secondactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSwitchClicked(View view){
+        Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+        intent.putExtra("Name","Rinko");
+        intent.putExtra("Age",18);
+        startActivity(intent);
     }
 }
